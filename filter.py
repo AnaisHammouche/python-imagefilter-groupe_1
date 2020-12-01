ec20595ba3f5985322aa72930cbd38c45a0e3611
Walid <walidhaddoury@outlook.fr>
2020-12-01 15:33:05 +0100

    ajout de nouvelles images

--- filter.py
+++ filter.py
@@ -2,9 +2,11 @@ import cv2
 
 
 def filter_gray_scale():
-    image = cv2.imread('assets/maggie.jpg')
+    image = cv2.imread('assets/homer.jpeg')
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imshow('Original image', image)
     cv2.imshow('Gray image', gray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+filter_gray_scale()
