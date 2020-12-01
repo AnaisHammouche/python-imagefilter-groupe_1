31186c0e76a4685d1c260d900ceebde045a50449
Walid <walidhaddoury@outlook.fr>
2020-12-01 15:25:50 +0100

    gray scale marche

--- filter.py
+++ filter.py
@@ -1,5 +1,10 @@
 import cv2
 
-image = cv2.imread('lien image')
 
-gray_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def filter_gray_scale():
+    image = cv2.imread('assets/maggie.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Original image', image)
+    cv2.imshow('Gray image', gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
